2dcf8852021fa5c06cb572900768fecfee90fa30
jixxxu <yoojs1129@naver.com>
2023-05-03 13:28:44 +0900

    user

--- src/main/java/com/sstep/demo/dao/UserRepository.java
+++ src/main/java/com/sstep/demo/dao/UserRepository.java
@@ -1,9 +1,18 @@
 package com.sstep.demo.dao;
 
 import com.sstep.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository {
-    User save(User user);
-}
+public interface UserRepository extends JpaRepository<User, Long>
+
+    {
+        User save(User user);
+
+        User findByUSER_ID (String user_id); //이메일로 user 조회
+
+        List<User> findAll (); //전체 조회
+    }
--- src/main/java/com/sstep/demo/service/UserService.java
+++ src/main/java/com/sstep/demo/service/UserService.java
@@ -13,4 +13,5 @@ public class UserService {
     public User save(User user) {
         return userRepository.save(user);
     }
+
 }
