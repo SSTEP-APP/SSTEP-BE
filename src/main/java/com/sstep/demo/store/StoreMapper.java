3db5f3d0737e41db8b4eda8729fc47e679423a3a
heebum kim <heebum9955@naver.com>
2023-07-13 23:34:59 +0900

    fix: Store

--- src/main/java/com/sstep/demo/store/StoreMapper.java
+++ src/main/java/com/sstep/demo/store/StoreMapper.java
@@ -4,16 +4,11 @@ import com.sstep.demo.staff.domain.Staff;
 import com.sstep.demo.staff.dto.StaffRequestDto;
 import com.sstep.demo.store.domain.Store;
 import com.sstep.demo.store.dto.StoreRequestDto;
-import com.sstep.demo.store.dto.StoreResponseDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StoreMapper {
     Store toEntity(StoreRequestDto storeRequestDto);
-
-    StoreResponseDto EntityToResponseDto(Store store);
-
-    StoreRequestDto ResponseToRequestDto(StoreRequestDto  storeRequestDto);
 
     Staff toStaffEntity(StaffRequestDto staffRequestDto);
 }
