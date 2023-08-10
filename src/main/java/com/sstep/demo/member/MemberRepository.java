473fd08f18e79732a491892575fbdf54954a8f4c
heebum kim <heebum9955@naver.com>
2023-08-10 22:50:05 +0900

    fix: Member Repository

--- src/main/java/com/sstep/demo/member/MemberRepository.java
+++ src/main/java/com/sstep/demo/member/MemberRepository.java
@@ -3,9 +3,11 @@ package com.sstep.demo.member;
 
 import com.sstep.demo.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> { //<Entity 클래스, PK 타입>
+    @Query("SELECT m FROM Member m WHERE m.username = :username")
     Member findByUsername(String username); //아이디로 회원찾기
 }
