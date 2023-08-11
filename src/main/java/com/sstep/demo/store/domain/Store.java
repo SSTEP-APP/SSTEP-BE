2c7cd9bee94c9d6d72168835e68be1181a25e1c7
heebum kim <heebum9955@naver.com>
2023-08-11 10:13:25 +0900

    fix: domain

--- src/main/java/com/sstep/demo/store/domain/Store.java
+++ src/main/java/com/sstep/demo/store/domain/Store.java
@@ -17,19 +17,12 @@ public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id; //사업장 고유번호
-    @Column(nullable = false)
     private String name; //사업장 이름
-    @Column(nullable = false)
     private String address; //사업장 주소
-    @Column(nullable = false)
     private String latitude; //사업장 위도 좌표
-    @Column(nullable = false)
     private String longitude; //사업장 경도 좌표
-    @Column(nullable = false)
     private boolean scale; //사업장 규모(5인이상: T, 이하: F)
-    @Column(nullable = false)
     private boolean plan; //사업장 유료플랜 여부
-    @Column(nullable = false, unique = true)
     private long code; //사업장 코드번호 => 인앱 사업장 검색시 사용
 
     public void setName(String name) {
