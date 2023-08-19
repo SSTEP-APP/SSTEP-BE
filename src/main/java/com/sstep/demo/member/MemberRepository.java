e558c0f0782afa4e7f1b94a39bcdb7e9fd8fc5b9
heebum kim <heebum9955@naver.com>
2023-08-19 20:18:29 +0900

    feat: Member 이름과 전화번호로 회원 찾기 기능

--- src/main/java/com/sstep/demo/member/MemberRepository.java
+++ src/main/java/com/sstep/demo/member/MemberRepository.java
@@ -10,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member, Long> { //<Entity 클래스, PK 타입>
     @Query("SELECT m FROM Member m WHERE m.username = :username")
     Member findByUsername(String username); //아이디로 회원찾기
+
+    @Query("SELECT m FROM Member m WHERE m.name = :name and m.phoneNum = :phoneNum")
+    Member findByNameAndPhoneNum(String name, String phoneNum); //이름과 전화번호로 회원 찾기
 }
