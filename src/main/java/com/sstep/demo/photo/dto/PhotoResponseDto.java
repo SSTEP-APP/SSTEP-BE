a8a723f59d3eabbaae04fc3bc963e76381ba5482
heebum kim <heebum9955@naver.com>
2023-09-04 14:29:06 +0900

    fix: 사진 업로드 기능 수정

--- src/main/java/com/sstep/demo/photo/dto/PhotoResponseDto.java
+++ src/main/java/com/sstep/demo/photo/dto/PhotoResponseDto.java
@@ -1,5 +1,7 @@
 package com.sstep.demo.photo.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 @Builder
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PhotoResponseDto {
     private long id;
     private String fileName;
