aad36b2da72d3097bf75e7504426352650dca824
heebum kim <heebum9955@naver.com>
2023-09-05 21:04:50 +0900

    fix: 체크리스트 DTO 수정

--- src/main/java/com/sstep/demo/checklist/dto/CheckListRequestDto.java
+++ src/main/java/com/sstep/demo/checklist/dto/CheckListRequestDto.java
@@ -13,16 +13,14 @@ import java.util.Set;
 @Getter
 @Builder
 public class CheckListRequestDto {
-    private long id; //체크 리스트 고유번호
     private String title; //체크 리스트 제목
     private String date; //체크 리스트 작성 일자
     private String contents; //체크 리스트 내용
     private String endDay; //체크 리스트 마감 일자
+    private String categoryName; //카테고리 명
+    private Set<CheckListManagerRequestDto> checkListManagersRequestDto; //체크 리스트 담당자 배열
     private boolean needPhoto; //체크 리스트 사진 필수 여부
     private boolean isComplete; //체크 리스트 완료 여부
     private String memo; //체크 리스트 완료 시 메모
     private long[] photoId; //사진 고유 번호 배열
-    private long categoryId; //카테고리 고유번호
-    private String categoryName; //카테고리 명
-    private Set<CheckListManagerRequestDto> checkListManagersRequestDto; //체크 리스트 담당자 배열
 }
