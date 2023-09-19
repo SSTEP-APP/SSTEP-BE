cf48359dc78a2d0df14e462835ea16ea67cef834
heebum kim <heebum9955@naver.com>
2023-09-19 20:05:46 +0900

    fix: HealthDoc 보건증 등록 직원 조회 기능 수정

--- src/main/java/com/sstep/demo/healthdoc/dto/HealthDocResponseDto.java
+++ src/main/java/com/sstep/demo/healthdoc/dto/HealthDocResponseDto.java
@@ -11,7 +11,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 public class HealthDocResponseDto {
-    private long id; //문서 고유번호
+    private Long id; //문서 고유번호
+    private Long staffId; //직원 고유번호
     private String name; //직원명
     private String checkUpDate; //보건증 검진일
     private String expirationDate; //보건증 만료일
