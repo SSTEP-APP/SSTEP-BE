144888d4e425a075348699d614aeebc5e77abbd2
heebum kim <heebum9955@naver.com>
2023-09-20 16:54:21 +0900

    fix: Calendar 직원 조회 기능 수정

--- src/main/java/com/sstep/demo/calendar/CalendarRepository.java
+++ src/main/java/com/sstep/demo/calendar/CalendarRepository.java
@@ -14,8 +14,11 @@ public interface CalendarRepository extends JpaRepository<Calendar, Long> {
     @Query("SELECT c FROM Calendar c WHERE c.staff.id = :staffId")
     Set<Calendar> findCalendarsByStaffId(Long staffId);
 
-    //캘린더에 별도 추가로 추가 근무하는 직원 리스트 + 스케줄에 등록해 고정으로 근무하는 직원 리스트
-    @Query("SELECT DISTINCT s, sc FROM Calendar c JOIN c.staff s LEFT JOIN Schedule sc ON c.staff.id = sc.staff.id " +
-            "WHERE (c.staff.store.id = :storeId and c.calendarDate = :date) or (sc.weekDay = :day and sc.staff.store.id = :storeId)")
-    Set<Staff> findDayWorkStaffsByDate(Long storeId, String date, DayOfWeek day);
+    //캘린더에 별도 추가로 추가 근무하는 직원 리스트
+    @Query("SELECT c.staff FROM Calendar c WHERE c.staff.store.id = :storeId and c.calendarDate = :date")
+    Set<Staff> findDayWorkStaffsByDate(Long storeId, String date);
+
+    //스케줄에 등록해 고정으로 근무하는 직원 리스트
+    @Query("SELECT s.staff FROM Schedule s WHERE s.staff.store.id = :storeId and s.weekDay = :day")
+    Set<Staff> findDayWorkStaffsByDay(Long storeId, DayOfWeek day);
 }
